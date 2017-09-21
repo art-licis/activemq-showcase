141a09a5072fcbeb15e443fcf4c8cb7e74ca8e28
art-licis <arturs.licis@gmail.com>
2017-09-21 16:15:34 +0300

    Simple embedded broker example (via xbean config)

---
+++ src/main/java/today/jvm/amq/showcase/EmbeddedXmlBrokerFactory.java
@@ -0,0 +1,24 @@
+package today.jvm.amq.showcase;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerFactory;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.io.File;
+import java.util.Hashtable;
+
+public class EmbeddedXmlBrokerFactory {
+	public static ActiveMQConnectionFactory createConnectionFactory(String xbeanUri) throws NamingException, JMSException {
+		Hashtable<String, String> properties = new Hashtable<String, String>();
+		properties.put("java.naming.factory.initial", "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+		properties.put(Context.PROVIDER_URL, "vm://localhost?brokerConfig=xbean:" + xbeanUri);
+		InitialContext initialContext = new InitialContext(properties);
+		ActiveMQConnectionFactory connectionFactory = (ActiveMQConnectionFactory) initialContext.lookup("ConnectionFactory");
+
+		return connectionFactory;
+	}
+}
---
+++ src/main/java/today/jvm/amq/showcase/SimpleBrokerShowcase.java
@@ -0,0 +1,36 @@
+package today.jvm.amq.showcase;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import javax.naming.NamingException;
+import java.io.File;
+
+public class SimpleBrokerShowcase {
+	public static void main(String[] args) throws JMSException, NamingException, InterruptedException {
+		ActiveMQConnectionFactory connectionFactory = EmbeddedXmlBrokerFactory.createConnectionFactory("today/jvm/amq/showcase/activemq-simple.xml");
+
+		Connection connection = connectionFactory.createConnection();
+		connection.start();
+
+		Session producerSession = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		MessageProducer producer = producerSession.createProducer(producerSession.createTopic("amq.simple.topic"));
+
+		Session consumerSession = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		MessageConsumer consumer = producerSession.createConsumer(producerSession.createTopic("amq.simple.topic"));
+		consumer.setMessageListener(new MessageListener() {
+			public void onMessage(Message message) {
+				System.out.println("Received a message: " + message);
+			}
+		});
+
+		producer.send(producerSession.createTextMessage("This is a first message's payload"));
+
+		Thread.sleep(100);
+
+		producerSession.close();
+		consumerSession.close();
+
+		connection.close();
+	}
+}
